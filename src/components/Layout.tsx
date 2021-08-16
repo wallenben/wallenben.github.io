b65aa8b0ea00b26c27de2ec059457b5f37c5463c
ben <pbjpie@outlook.com>
2021-08-16 04:37:39 -0500

    Typescript implementation
    quick hotfix that went  under the radar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,10 +15,10 @@ const Layout = ({ children }) => {
       <div className="flex flex-col min-h-screen bg-gray-200 dark:bg-gray-600">
         <header className="p-4  bg-gray-500 dark:bg-gray-800 text-white font-semibold">
         <div class="flex justify-start">
-        <h1 className="max-1w-2xl mx-2"><Link to="/">Ben Wallen</Link></h1>
-        <h1 className="max-w-2xl mx-8"><Link to="/blog">Blog</Link></h1>
-        <h1 className="max-w-2xl mx-8"><Link to="/about">About</Link></h1>
-        <h1 className="max-w-2xl mx-8"><Link to="/resume">Resume</Link></h1>
+        <h1 className="max-1w-2xl mx-4"><Link to="/">Ben Wallen</Link></h1>
+        <h1 className="max-w-2xl mx-3"><Link to="/blog">Blog</Link></h1>
+        <h1 className="max-w-2xl mx-3"><Link to="/about">About</Link></h1>
+        <h1 className="max-w-2xl mx-3"><Link to="/resume">Resume</Link></h1>
         </div>
         </header>
         {children}
