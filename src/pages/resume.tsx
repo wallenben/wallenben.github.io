21ce3436772f3e27b515ad94cde3417237e14354
pbjpie <pbjpie@outlook.com>
2022-02-09 22:40:55 -0600

    resume fixes, global scope tweaks for gatsby

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,25 +1,22 @@
 import React from "react"
-import Layout from "../components/Layout" 
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-import pdf from './Resume.pdf'
-const IndexPage = () => {
+import Layout from "../components/Layout"
+import { Document, Page } from "react-pdf"
+import { StaticImage } from "gatsby-plugin-image"
+const ResumePage = () => {
   return (
     <Layout>
       <main className="max-w-3xl flex-grow mx-auto flex flex-col justify-around bg-gray-50 dark:bg-gray-700 rounded-xl p-6 m-3">
         <div className="sm:flex sm:flex-row-reverse sm:items-center">
           <div className="sm:px-2"></div>
           <div>
-           <Document file={pdf}>
-           <Page pageNumber={1} />
-           </Document>
+              <StaticImage src="../images/resume.png" alt="Click to download." />
           </div>
-          </div>
-         
-         
-       </main>
-     
+        </div>
+
+
+      </main>
+
     </Layout>
   )
 }
-export default IndexPage;
+export default ResumePage;
