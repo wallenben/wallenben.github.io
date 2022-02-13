322153cbdc619e2b25176839db8e230ae1ee78e6
pbjpie <pbjpie@outlook.com>
2022-02-13 17:07:43 -0600

    updated tailwind, added github markdown styling

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -2,24 +2,26 @@ import { graphql } from "gatsby"
 import React from "react"
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from "../../components/Layout"
+import "github-markdown-css/github-markdown.css"
 const MDXPage = ({ data }) => {
   return (
     <Layout>
-      <main className="max-w-4xl flex-grow  mx-auto flex flex-col justify-around bg-gray-50 dark:bg-gray-700 text-green-600 dark:text-green-300 rounded-xl p-6 m-3">
-      <div className="flex-grow 0">
-      <h1>{data.mdx.frontmatter.title}</h1>
-      <h3 className="text-sm text-green-400">{data.mdx.frontmatter.date}</h3>
-      <br />
-      <div className="  sm:items-center text-gray-700 dark:text-white flex-grow ">
-          <div className="sm:px-7">
-            <MDXRenderer>
-              {data.mdx.body}
-            </MDXRenderer>
-          </div>
-
+      <main className="max-w-4xl flex-grow  mx-auto flex flex-col justify-around bg-white dark:bg-gray-700 text-green-600 dark:text-green-300 rounded-xl p-6 m-3">
+        <div className="flex-grow 0">
+          <h1>{data.mdx.frontmatter.title}</h1>
+          <h3 className="text-sm text-green-400">{data.mdx.frontmatter.date}</h3>
+          <br />
+          <article className="prose lg:prose-xl">
+            <div className="sm:items-center text-gray-700 dark:text-white flex-grow ">
+              <div className="markdown-body sm:px-7">
+                <MDXRenderer>
+                  {data.mdx.body}
+                </MDXRenderer>
+              </div>
+            </div>
+          </article>
         </div>
-      </div>
-       
+
       </main>
     </Layout>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    
     extend: {},
+    listStyleType: {
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    require('@tailwindcss/typography'),
+    'gatsby-plugin-postcss',
+  ]
 }
